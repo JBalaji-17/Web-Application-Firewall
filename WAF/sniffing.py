192131180df7047b5be119f6d4f3e2b85d04816e
JBalaji_17 <115694744+JBalaji-17@users.noreply.github.com>
2024-09-09 22:06:21 +0530

    Update sniffing.py

--- WAF/sniffing.py
+++ WAF/sniffing.py
@@ -1,90 +1,32 @@
-'''Implementation of the sniffing application that uses classification module for WAF implementation.'''
-
 from scapy.all import sniff, Raw
-import scapy.all as scapy
-from scapy.layers.http import HTTPRequest, HTTP
-from scapy.layers.inet import IP, TCP
-from scapy.sessions import IPSession, TCPSession
-import urllib.parse
+from scapy.layers.http import HTTPRequest
+from scapy.layers.inet import IP
+from scapy.sessions import TCPSession
 from request import Request, DBController
 from classifier import ThreatClassifier
 from argparse import ArgumentParser
+import urllib.parse
 
+# Define command-line arguments
 parser = ArgumentParser()
-parser.add_argument('--port', type=int, default=5000, help='Defines which port to sniff')
+parser.add_argument('--port', type=int, default=80, help='Port number to sniff HTTP traffic on')
 
 args = parser.parse_args()
 
-scapy.packet.bind_layers(TCP, HTTP, dport=args.port)
-scapy.packet.bind_layers(TCP, HTTP, sport=args.port)
-
+# Initialize DB controller and ThreatClassifier
 db = DBController()
 threat_clf = ThreatClassifier()
 
-header_fields = ['Http_Version',
-'A_IM',
-'Accept',
-'Accept_Charset',
-'Accept_Datetime',
-'Accept_Encoding', 
-'Accept_Language',
-'Access_Control_Request_Headers',
-'Access_Control_Request_Method',
-'Authorization',
-'Cache_Control',
-'Connection',
-'Content_Length',
-'Content_MD5',
-'Content_Type',
-'Cookie',
-'DNT',
-'Date',
-'Expect',
-'Forwarded',
-'From',
-'Front_End_Https',
-'If_Match',
-'If_Modified_Since',
-'If_None_Match',
-'If_Range',
-'If_Unmodified_Since',
-'Keep_Alive',
-'Max_Forwards',
-'Origin',
-'Permanent',
-'Pragma',
-'Proxy_Authorization',
-'Proxy_Connection',
-'Range',
-'Referer',
-'Save_Data',
-'TE',
-'Upgrade',
-'Upgrade_Insecure_Requests',
-'User_Agent',
-'Via',
-'Warning',
-'X_ATT_DeviceId',
-'X_Correlation_ID',
-'X_Csrf_Token',
-'X_Forwarded_For',
-'X_Forwarded_Host',
-'X_Forwarded_Proto',
-'X_Http_Method_Override',
-'X_Request_ID',
-'X_Requested_With',
-'X_UIDH',
-'X_Wap_Profile']
+header_fields = [...]
 
 def get_header(packet):
     headers = {}
     for field in header_fields:
         f = getattr(packet[HTTPRequest], field)
-        if f != None and f != 'None':
+        if f is not None and f != 'None':
             headers[field] = f.decode()
 
     return headers
-
 
 def sniffing_function(packet):
     if packet.haslayer(HTTPRequest):
@@ -104,11 +46,11 @@ def sniffing_function(packet):
         if packet.haslayer(Raw):
             req.body = packet[Raw].load.decode()
 
-        #print('Request')
-
         threat_clf.classify_request(req)
-
         db.save(req)
 
-pkgs = sniff(prn = sniffing_function, iface='lo', filter = 'port ' + str(args.port) + ' and inbound', session = TCPSession)
+# Start sniffing
+pkgs = sniff(prn=sniffing_function, filter=f'tcp dst port {args.port}', session=TCPSession)
+
+# Close DB connection
 db.close()
